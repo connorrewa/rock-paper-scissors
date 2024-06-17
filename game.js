2b81045d23d46e2e6413a5e12827e99016d2dc43
connorrewa <connorrewa13@hotmail.com>
2024-06-17 19:51:28 -0400

    add rough ui

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -10,48 +14,92 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose rock, paper, or scissors")
-    return choice.toLowerCase();
 
+
+function playRound(humanChoice) {
+    console.log("playing...");
+    let res;
+    let computerChoice = getComputerChoice();
+    res = calculateRoundWinner(humanChoice, computerChoice);
+    announceRoundWinner(res, humanChoice, computerChoice);
 }
 
-
-
-function playRound(humanChoice, computerChoice) {
-    let res;
+function calculateRoundWinner(humanChoice, computerChoice) {
     if (humanChoice == "rock") {
-        if (computerChoice == "rock") res = "tie! try again.";
-        else if (computerChoice == "paper") res = "you lose!";
-        else res = "you win!";
+        if (computerChoice == "rock") {
+            res = "tie! try again.";
+        } else if (computerChoice == "paper") {
+            res = "you lose!";
+            computerScore++;
+        } else {
+            res = "you win!";
+            humanScore++;
+        }
     } else if (humanChoice == "paper") {
-        if (computerChoice == "rock") res = "you win!";
-        else if (computerChoice == "paper") res = "tie! try again.";
-        else res = "you lose!";
+        if (computerChoice == "rock") {
+            res = "you win!";
+            humanScore++;
+        } else if (computerChoice == "paper") {
+            res = "tie! try again.";
+        } else {
+            res = "you lose!";
+            computerScore++;
+        }
     } else {
-        if (computerChoice == "rock") res = "you lose!";
-        else if (computerChoice == "paper") res = "you win!";
-        else res = "tie! try again.";
+        if (computerChoice == "rock") {
+            res = "you lose!";
+            computerScore++;
+        } else if (computerChoice == "paper") {
+            res = "you win!";
+            humanScore++;
+        } else {
+            res = "tie! try again.";
+        }
     }
-    console.log(res + `\nyou chose ${humanChoice} and the computer got ${computerChoice}`)
     return res;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        let result = playRound(humanChoice, computerChoice);
-        if (result == "you win!") humanScore++;
-        else if (result == "you lose!") computerScore++;
-        console.log(humanScore, computerScore);
+function announceRoundWinner(res, humanChoice, computerChoice) {
+    if (humanScore === 5 || computerScore ===5) {
+        announceWinner();
+        return;
     }
+    div = document.querySelector("#results");
+    div.textContent = `${res} \nyou chose ${humanChoice} and the computer got ${computerChoice}.\n`;
+    div.textContent += `You: ${humanScore} - CPU: ${computerScore}\n`
+
 }
 
 
-playGame();
 
+function announceWinner() {
+    div = document.querySelector("#results")
+    if (humanScore === 5) {
+        div.textContent = `Congratulations! You win!`;
+    } else {
+        div.textContent = `You lost... Try again :(`;
+    }
+    humanScore = 0;
+    computerScore = 0;
+    div.textContent += "  Ready for more?";
+}
 
+rockButton = document.querySelector("#rock");
+paperButton = document.querySelector("#paper");
+scissorButton = document.querySelector("#scissor");
+
+rockButton.addEventListener("click", () => {
+    console.log("rock pressed");
+    if (humanScore < 5 && computerScore < 5)
+        playRound("rock");
+});
+paperButton.addEventListener("click", () => {
+    console.log("paper pressed");
+    if (humanScore < 5 && computerScore < 5)
+        playRound("paper");
+});
+scissorButton.addEventListener("click", () => {
+    console.log("scissor pressed");
+    if (humanScore < 5 && computerScore < 5)
+        playRound("scissor")
+});
